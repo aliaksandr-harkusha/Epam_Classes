a7a7d07b057cf012584dae60372be5a56f0983e5
Mafyak <Huba.est@gmail.com>
2018-02-02 07:03:05 -0500

    Update Solution.java

--- Pre-Training/Task 1/Solution.java
+++ Pre-Training/Task 1/Solution.java
@@ -24,7 +24,7 @@ public class Solution {
         return grams / KILOS_TO_GRAMS;
     }
 
-    // 1. Задача №1 - Динозавр - в килограмы
+    // 1. Задача №1 - Динозавр - в тонны
     public double calculatorDinoMassToTons(double grams) {
         return grams / TONS_TO_GRAMS;
     }
