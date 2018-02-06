b23e53ef328c86ec2334895b18502306d60c4247
Mafyak <Huba.est@gmail.com>
2018-02-06 13:40:38 -0500

    Create Manipulator6DigitsNumber.java

---
+++ Pre-Training/Task 1/Manipulator6DigitsNumber.java
@@ -0,0 +1,32 @@
+package Task1;
+
+public class Manipulator6DigitsNumber {
+    
+    static double LENGTH_OF_6_DIGITS_NUMBER = 6.0;
+
+    //6. Задача №6 - Арифметическое среднее цифр 6тизначного числа
+    public double arithAveOf6DigitsNumber(int number6Digits) {
+
+        int d6 = number6Digits % 10;
+        int d5 = (number6Digits /= 10) % 10;
+        int d4 = (number6Digits /= 10) % 10;
+        int d3 = (number6Digits /= 10) % 10;
+        int d2 = (number6Digits /= 10) % 10;
+        int d1 = (number6Digits /= 10) % 10;
+
+        return (d1 + d2 + d3 + d4 + d5 + d6) / LENGTH_OF_6_DIGITS_NUMBER;
+    }
+
+    //6. Задача №6 - Геометрическое среднее цифр 6тизначного числа
+    public double arithGeoOf6DigitsNumber(int number6Digits) {
+
+        int d6 = number6Digits % 10;
+        int d5 = (number6Digits /= 10) % 10;
+        int d4 = (number6Digits /= 10) % 10;
+        int d3 = (number6Digits /= 10) % 10;
+        int d2 = (number6Digits /= 10) % 10;
+        int d1 = (number6Digits /= 10) % 10;
+
+        return Math.pow(d1 * d2 * d3 * d4 * d5 * d6, 1.0 / LENGTH_OF_6_DIGITS_NUMBER);
+    }
+}
