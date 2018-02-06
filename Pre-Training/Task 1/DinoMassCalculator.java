40866013d6552d7dea985e9ad839ef181b2bbc24
Mafyak <Huba.est@gmail.com>
2018-02-06 13:35:26 -0500

    Create DinoMassCalculator.java

---
+++ Pre-Training/Task 1/DinoMassCalculator.java
@@ -0,0 +1,17 @@
+package Task1;
+
+public class DinoMassCalculator {
+    
+    static int KILOS_TO_GRAMS = 1000;
+    static int TONS_TO_GRAMS = 1_000_000;
+    
+    // 1. Задача №1 - Динозавр - в килограмы
+    public double calculatorDinoMassToKilos(double grams) {
+        return grams / KILOS_TO_GRAMS;
+    }
+
+    // 1. Задача №1 - Динозавр - в тонны
+    public double calculatorDinoMassToTons(double grams) {
+        return grams / TONS_TO_GRAMS;
+    }
+}
