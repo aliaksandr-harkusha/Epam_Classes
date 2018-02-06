8d468cc2350d04ecddbe2934ad6ccdd9f169d659
Mafyak <Huba.est@gmail.com>
2018-02-06 15:46:49 -0500

    Create NokNumber.java

---
+++ Pre-Training/Task 3/Model/NokNumber.java
@@ -0,0 +1,9 @@
+package test.Crap;
+
+public class NokNumber {
+
+    //найти НОК двух натуральных чисел a и b.
+    private static int nokNumber(int numberOne, int numberTwo) {
+        return (numberOne * numberTwo) / NodNumber.nodNumber(numberOne, numberTwo);
+    }
+}
