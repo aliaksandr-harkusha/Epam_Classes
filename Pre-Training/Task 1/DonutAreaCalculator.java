7bbbfaf07c9a572c54f32fd99f2b3d176560f671
Mafyak <Huba.est@gmail.com>
2018-02-06 13:36:48 -0500

    Create DonutAreaCalculator.java

---
+++ Pre-Training/Task 1/DonutAreaCalculator.java
@@ -0,0 +1,14 @@
+package test;
+
+public class DonutAreaCalculator {
+
+    //2. Задача №2 - Площадь кольца
+    public double calculatorDonutArea(double radius1, double radius2) {
+        return Math.PI * (calculatorSquare(radius1) - calculatorSquare(radius2));
+    }
+
+    //method-helper for Task2
+    public double calculatorSquare(double number) {
+        return number * number;
+    }
+}
