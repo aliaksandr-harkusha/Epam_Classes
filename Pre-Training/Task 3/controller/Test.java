8e810011433597f369b770401fceb62399416756
Mafyak <Huba.est@gmail.com>
2018-02-06 16:00:22 -0500

    Create Test.java

---
+++ Pre-Training/Task 3/controller/Test.java
@@ -0,0 +1,11 @@
+package task3.controller;
+import task3.model.*;
+import task3.view.View;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int number = 194221;
+        HeadsOrTails.coinFlipper();
+    }
+}
