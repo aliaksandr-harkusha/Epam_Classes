4c6cecdef3ecfcf31cac627476840fe36f39144a
Mafyak <Huba.est@gmail.com>
2018-02-06 13:39:49 -0500

    Create Manipulator5DigitsNumbers.java

---
+++ Pre-Training/Task 1/Manipulator5DigitsNumbers.java
@@ -0,0 +1,29 @@
+package Task1;
+
+public class Manipulator5DigitsNumbers {
+
+    //5. Задача №5 - Сумма цифр пятизначного числа
+    public int calculatorSumOf5DigitsNumber(int number5Digits) {
+
+        int d5 = (number5Digits /= 10) % 10;
+        int d4 = (number5Digits /= 10) % 10;
+        int d3 = (number5Digits /= 10) % 10;
+        int d2 = (number5Digits /= 10) % 10;
+        int d1 = (number5Digits /= 10) % 10;
+
+        return d1 + d2 + d3 + d4 + d5;
+    }
+
+
+    //5. Задача №5 - Произведение цифр пятизначного числа
+    public int calculatorMultiplOf5DigitsNumber(int number5Digits) {
+
+        int d5 = (number5Digits /= 10) % 10;
+        int d4 = (number5Digits /= 10) % 10;
+        int d3 = (number5Digits /= 10) % 10;
+        int d2 = (number5Digits /= 10) % 10;
+        int d1 = (number5Digits /= 10) % 10;
+
+        return d1 * d2 * d3 * d4 * d5;
+    }
+}
