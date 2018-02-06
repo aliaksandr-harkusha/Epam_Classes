18da0151d9f0ef30efc22ad9a21d8359ff2e1f42
Mafyak <Huba.est@gmail.com>
2018-02-06 12:43:55 -0500

    Create MoodSensor.java

---
+++ Pre-Training/Task 2/MoodSensor.java
@@ -0,0 +1,29 @@
+package Task2;
+
+public class MoodSensor {
+
+    public void moodSensor() {
+        int moodScale = (int) (Math.random() * 5);
+        switch (moodScale) {
+            case 0:
+                System.out.println("(×_×)");
+                break;
+            case 1:
+                System.out.println("(╯︵╰,)");
+                break;
+            case 2:
+                System.out.println("(￢_￢)");
+                break;
+            case 3:
+                System.out.println("(─‿‿─)");
+                break;
+            case 4:
+                System.out.println("(◕‿◕)");
+                break;
+            case 5:
+                System.out.println("＼(￣▽￣)／");
+                break;
+        }
+
+    }
+}
