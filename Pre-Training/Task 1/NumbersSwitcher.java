9bcae497094dcd94a62c8b509259635c51765ae7
Mafyak <Huba.est@gmail.com>
2018-02-06 13:37:31 -0500

    Create NumbersSwitcher.java

---
+++ Pre-Training/Task 1/NumbersSwitcher.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class NumbersSwitcher {
+
+    //3. Задача №3 - Переброс переменных
+    public void intSwitcher(int a, int b) {
+        System.out.println("Задача №3 - Переброс переменных " + a + " и " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Новая переменная a = " + a + ", новая переменная b = " + b + "\n");
+    }
+}
