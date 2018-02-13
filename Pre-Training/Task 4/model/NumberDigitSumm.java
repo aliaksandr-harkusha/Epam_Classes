8816401e8317006f96a9380552974113d747ba10
Mafyak <Huba.est@gmail.com>
2018-02-13 15:49:06 -0500

    Update NumberDigitSumm.java

--- Pre-Training/Task 4/model/NumberDigitSumm.java
+++ Pre-Training/Task 4/model/NumberDigitSumm.java
@@ -1,1 +1,17 @@
-test
+package view;
+
+public class NumberDigitSumm {
+
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+    public static void print(boolean msg) {
+        System.out.println(msg);
+    }
+    public static void print(int msg) {
+        System.out.println(msg);
+    }
+    public static void print(long msg) {
+        System.out.println(msg);
+    }
+}
