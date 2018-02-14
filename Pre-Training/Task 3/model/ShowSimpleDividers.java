f2192728a6517bc993b0159632a8c006cf3afa5d
Mafyak <Huba.est@gmail.com>
2018-02-14 08:40:12 -0500

    Update ShowSimpleDividers.java

--- Pre-Training/Task 3/model/ShowSimpleDividers.java
+++ Pre-Training/Task 3/model/ShowSimpleDividers.java
@@ -3,11 +3,12 @@ package model;
 public class ShowSimpleDividers {
 
     //найти все простые делители заданного натурального числа
-    public static void showSimpleDividers(int numberToCheck) {
+    public static void calculateSimpleDividers(int numberToCheck) {
 
+        int half = numberToCheck/2;
         System.out.print("Simple dividers: ");
-        for (int i = 1; i <= numberToCheck; i++)
-            if (IsNumberSimple.isNumberSimple(i) && (numberToCheck % i == 0))
+        for (int i = 1; i <= half; i++)
+            if (IsNumberSimple.checkIfNumberSimple(i) && (numberToCheck % i == 0))
                 System.out.print(i + " ");
     }
 }
