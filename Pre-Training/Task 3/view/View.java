f3bc283ff80f01c19e053ee046c9a2dbe108824f
Mafyak <Huba.est@gmail.com>
2018-02-14 11:13:21 +0300

    Update View.java

--- Pre-Training/Task 3/view/View.java
+++ Pre-Training/Task 3/view/View.java
@@ -2,23 +2,7 @@ package view;
 
 public class View {
     
-    
     public static void print(Object msg) {
         System.out.println(msg);
     }
-
-    /*
-    public static void print(String msg) {
-        System.out.println(msg);
-    }
-    public static void print(boolean msg) {
-        System.out.println(msg);
-    }
-    public static void print(int msg) {
-        System.out.println(msg);
-    }
-    public static void print(long msg) {
-        System.out.println(msg);
-    }
-    */
 }
