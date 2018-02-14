cecf1c6cb9f1d4cd9e623ef1a84c0f178c191241
Mafyak <Huba.est@gmail.com>
2018-02-14 08:41:12 -0500

    Update NokNumber.java

--- Pre-Training/Task 3/model/NokNumber.java
+++ Pre-Training/Task 3/model/NokNumber.java
@@ -3,7 +3,7 @@ package model;
 public class NokNumber {
 
     //найти НОК двух натуральных чисел a и b.
-    public static long getNokNumber(int numberOne, int numberTwo) {
-        return (numberOne * numberTwo) / NodNumber.getNodNumber(numberOne, numberTwo);
+    public static long calculateNokNumber(int numberOne, int numberTwo) {
+        return (numberOne * numberTwo) / NodNumber.calculateNodNumber(numberOne, numberTwo);
     }
 }
