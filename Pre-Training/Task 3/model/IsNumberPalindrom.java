c8cd13914817bb725b14cc9f9ffb6ecf4a9c4d0c
Mafyak <Huba.est@gmail.com>
2018-02-14 08:41:57 -0500

    Update IsNumberPalindrom.java

--- Pre-Training/Task 3/model/IsNumberPalindrom.java
+++ Pre-Training/Task 3/model/IsNumberPalindrom.java
@@ -3,7 +3,7 @@ package model;
 public class IsNumberPalindrom {
 
     //проверить, является ли заданное натуральное число палиндромом
-    public static boolean isNumberPalindrom(int numberToCheck) {
+    public static boolean checkIfNumberPalindrom(int numberToCheck) {
         String stringNumberToCheck = Integer.toString(numberToCheck); //перевод значения в строку
         StringBuilder reverser = new StringBuilder(stringNumberToCheck).reverse(); //разворот строки
         String reverseNumber = new String(reverser.toString());
