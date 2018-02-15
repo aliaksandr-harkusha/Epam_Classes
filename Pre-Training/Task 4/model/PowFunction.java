694746ae586d4a9963990085227a36e70a173b62
Mafyak <Huba.est@gmail.com>
2018-02-15 16:51:42 -0500

    Update PowFunction.java

--- Pre-Training/Task 4/model/PowFunction.java
+++ Pre-Training/Task 4/model/PowFunction.java
@@ -1,12 +1,10 @@
-public class PowFunction {
+package model;
 
-    public static void main(String[] args) {
-        System.out.println(pow(0, 1));
-    }
+public class PowFunction {
 
     public static double pow(double number, int number2) {
         if (number == 0) {
-            System.out.println("0 - некорректное число, возвращаю 0 целых, 0 десятых");
+            System.out.println("Please, don't use 0 as a first input number");
             return 0;
         } else if (number2 == 0) return 1;
         return number * pow(number, number2 - 1);
