a439e4f02cf420b4092a3d2b161d098d5050b19d
Mafyak <Huba.est@gmail.com>
2018-02-16 11:30:24 -0500

    Update HanoiTower.java

--- Pre-Training/Task 4/model/HanoiTower.java
+++ Pre-Training/Task 4/model/HanoiTower.java
@@ -5,12 +5,8 @@ public class HanoiTower {
     public static void moveMaker(int height, String poleOne, String poleTwo, String poleThree) {
         if (height >= 1) {
             moveMaker(height - 1, poleOne, poleThree, poleTwo);
-            movePrinter(poleOne, poleTwo);
+            View.print(poleOne + "-->" + poleTwo);
             moveMaker(height - 1, poleThree, poleTwo, poleOne);
         }
-    }
-
-    public static void movePrinter(String poleOne, String poleTwo) {
-        View.print(poleOne + "-->" + poleTwo);
     }
 }
