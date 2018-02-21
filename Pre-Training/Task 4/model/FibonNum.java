a0028956c4c0de6294d46cde15c1cfe3f3c0956d
Mafyak <Huba.est@gmail.com>
2018-02-21 14:36:17 +0300

    Update FibonNum.java

--- Pre-Training/Task 4/model/FibonNum.java
+++ Pre-Training/Task 4/model/FibonNum.java
@@ -1,12 +1,17 @@
 package model;
 
 public class FibonNum {
-
-    public static int calculatorFibonNum(int n1, int n2, int num) {
+    
+    public static int calculateFibonNum(int num) {
         if (num <= 0)
             return -1;
         else if (num == 1)
-            return n1;
-        else return fibonNum(n2, n1 + n2, num - 1);
+            return 0;
+        else return calculatorFibonNum(0, 1, num - 1);
+    }
+
+    public static int calculatorFibonNum(int n1, int n2, int n3) {
+        if (n3 == 0) return n1;
+        else return calculatorFibonNum(n2, n1 + n2, n3 - 1);
     }
 }
