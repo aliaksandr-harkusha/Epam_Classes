49443c8373daa0eae9c36d7e2c068ee248e59893
Mafyak <Huba.est@gmail.com>
2018-02-23 04:36:50 +0300

    Update View.java

--- Pre-Training/Task 5/View/View.java
+++ Pre-Training/Task 5/View/View.java
@@ -1,7 +1,7 @@
 package by.epam.preTraining.SiarheiHuba.tasks.task5.View;
 
 public class View {
-    public void view(Object o) {
+    public static void view(Object o) {
         System.out.println(o.toString());
     }
 }
