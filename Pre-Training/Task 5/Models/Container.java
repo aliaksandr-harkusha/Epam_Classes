87b4ab4ab9b49e1d63d5883730dd08fa001da564
Mafyak <Huba.est@gmail.com>
2018-02-23 03:14:04 +0300

    Update Container.java

--- Pre-Training/Task 5/Models/Container.java
+++ Pre-Training/Task 5/Models/Container.java
@@ -4,27 +4,12 @@ import java.util.Date;
 
 public class Container {
 
-    private int height;
-    private int length;
-    private int width;
     private String type;
     private int containerID = 0;
     private Date arrivalDate;
     private Date departureDate;
-
-    public int getContainerID() {
-        return containerID;
-    }
-
     private boolean isInsured;
     private String ShippingCompany;
-
-    public Container() {
-    }
-
-    public Container(String type) {
-        this.type = type;
-    }
 
     public Container(String type, Date arrivalDate, Date departureDate, boolean isInsured, String shippingCompany) {
         this.type = type;
@@ -34,5 +19,54 @@ public class Container {
         this.ShippingCompany = shippingCompany;
         this.containerID = containerID;
         containerID++;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getContainerID() {
+        return containerID;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public boolean isInsured() {
+        return isInsured;
+    }
+
+    public String getShippingCompany() {
+        return ShippingCompany;
+    }
+
+    public boolean equals(Container cont) {
+        if (!(this.type == cont.getType()))
+            return false;
+        else if (!(this.getArrivalDate().compareTo(cont.getArrivalDate()) == 0))
+            return false;
+        else if (!(this.getDepartureDate().compareTo(cont.getDepartureDate()) == 0))
+            return false;
+        else if (!(this.getShippingCompany().equals(cont.getShippingCompany())))
+            return false;
+        else if (!(this.isInsured() == cont.isInsured()))
+            return false;
+        else return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Container id: " + this.containerID + ", arrivalDate: " + arrivalDate + ", departureDate: " +
+                departureDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return 42 * (this.containerID + this.getType().hashCode() + this.getArrivalDate().hashCode());
     }
 }
