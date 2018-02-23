3953c86ec298c91f0d48e2438074e9fd26534574
Mafyak <Huba.est@gmail.com>
2018-02-23 04:36:33 +0300

    Update Calculator.java

--- Pre-Training/Task 5/Models/Calculator.java
+++ Pre-Training/Task 5/Models/Calculator.java
@@ -1,14 +1,19 @@
 package by.epam.preTraining.SiarheiHuba.tasks.task5.Models;
 
+import by.epam.preTraining.SiarheiHuba.tasks.task5.View.View;
+
 public class Calculator {
-    public static double getPercentageOfInsuredContainers(Container[] wHouse){
-        int ins = 0;
-        for (Container x: wHouse){
-            if (x.isInsured())
-                ins++;
+    public static double getPercentageOfInsuredContainers(Warehouse<Container> wHouse){
+        int ins = 0, total = 0;
+        int size = wHouse.getAmountOfSpots();
+        for (int i=0; i<size; i++){
+            try {
+                if (wHouse.getContainerByIndex(i).isInsured())
+                    ins++;
+                total++;
+            } catch (NullPointerException e){
+                View.view("Empty Spot");}
         }
-        return ins/wHouse.length;
+        return (double)ins/total*100;
     }
-
-
 }
