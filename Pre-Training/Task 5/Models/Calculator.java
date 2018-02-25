58bccedfc7e4aeeb1c2236b1e2b8cf87d8c3c3d3
Mafyak <Huba.est@gmail.com>
2018-02-25 22:06:40 +0300

    Update Calculator.java

--- Pre-Training/Task 5/Models/Calculator.java
+++ Pre-Training/Task 5/Models/Calculator.java
@@ -1,19 +1,48 @@
 package by.epam.preTraining.SiarheiHuba.tasks.task5.Models;
 
-import by.epam.preTraining.SiarheiHuba.tasks.task5.View.View;
-
 public class Calculator {
-    public static double getPercentageOfInsuredContainers(Warehouse<Container> wHouse){
+    public static double getPercentageOfInsuredContainers(Warehouse<ContainerUnit> wHouse) {
         int ins = 0, total = 0;
-        int size = wHouse.getAmountOfSpots();
-        for (int i=0; i<size; i++){
+        int size = wHouse.getNextLoadNumber();
+        for (int i = 0; i < size; i++) {
             try {
                 if (wHouse.getContainerByIndex(i).isInsured())
                     ins++;
                 total++;
-            } catch (NullPointerException e){
-                View.view("Empty Spot");}
+            } catch (NullPointerException e) {
+            }
         }
-        return (double)ins/total*100;
+        return ins / total * 100;
+    }
+
+    public static int getAmountOfVacantSpots(Warehouse<ContainerUnit> wHouse) {
+
+        int size = wHouse.getNextLoadNumber();
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            try {
+                if (wHouse.getContainerByIndex(i) != null)
+                    counter++;
+            } catch (NullPointerException e) {
+            }
+        }
+        return wHouse.getMaxWarehouseCapacity() - counter;
+    }
+
+    public static long getAverageStorageTime(Warehouse<ContainerUnit> wHouse) {
+        int size = wHouse.getNextLoadNumber();
+        long days = 0;
+        int counter = 0;
+        long total = 0;
+        for (int i = 0; i < size; i++) {
+            try {
+                days = (wHouse.getContainerByIndex(i).getDepartureDate().getTimeInMillis() -
+                        wHouse.getContainerByIndex(i).getArrivalDate().getTimeInMillis());
+                total = total + days / (24 * 60 * 60 * 1000);
+                counter++;
+            } catch (NullPointerException e) {
+            }
+        }
+        return total / counter;
     }
 }
