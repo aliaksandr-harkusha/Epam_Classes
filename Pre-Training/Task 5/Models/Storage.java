fbb4d0efce79feb90991c943879a45c5f5e31f37
Mafyak <Huba.est@gmail.com>
2018-02-25 22:07:36 +0300

    Update Storage.java

--- Pre-Training/Task 5/Models/Storage.java
+++ Pre-Training/Task 5/Models/Storage.java
@@ -3,7 +3,7 @@ package by.epam.preTraining.SiarheiHuba.tasks.task5.Models;
 public interface Storage<T>{
     boolean isFull();
     boolean isEmpty();
-    int size();
+    int getSize();
     void add(T unit);
     void removeByID(int index);
     boolean contains(T unit);
