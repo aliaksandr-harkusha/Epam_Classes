6ca370b37b2870d8a6a4ef0370859c6dba0777d9
Mafyak <Huba.est@gmail.com>
2018-02-25 22:07:54 +0300

    Update Warehouse.java

--- Pre-Training/Task 5/Models/Warehouse.java
+++ Pre-Training/Task 5/Models/Warehouse.java
@@ -1,115 +1,149 @@
 package by.epam.preTraining.SiarheiHuba.tasks.task5.Models;
 
-public class Warehouse<Container> implements Storage<Container>{
+import by.epam.preTraining.SiarheiHuba.tasks.task5.View.View;
+import java.util.Arrays;
 
-    private Container[] warehouse;
-    private int spotID = 0;
+public class Warehouse<ContainerUnit> implements Storage<ContainerUnit> {
+
+    private ContainerUnit[] warehouse;
+    private int loadNumber = 0;
+    private int spotsTaken = 0;
     private static int ARRAY_INCREASE_SIZE = 10;
+    private int maxWarehouseCapacity;
 
-    public Warehouse() {
-        warehouse = (Container[]) new Object[ARRAY_INCREASE_SIZE];
+    public Warehouse(int warehouseCapacity) {
+        warehouse = (ContainerUnit[]) new Object[ARRAY_INCREASE_SIZE];
+        this.maxWarehouseCapacity = warehouseCapacity;
     }
 
-    public Container[] getWarehouse() {
+    public Warehouse() {
+        warehouse = (ContainerUnit[]) new Object[ARRAY_INCREASE_SIZE];
+        this.maxWarehouseCapacity = 5000;
+    }
+
+    public int getTakenSpotsQuantity() {
+        return spotsTaken;
+    }
+
+    public int getVacantSpaces() {
+        return maxWarehouseCapacity - spotsTaken;
+    }
+
+    public ContainerUnit[] getWarehouse() {
         return warehouse;
     }
 
-    public Container getContainerByIndex(int index) {
-        return (Container) warehouse[index];
+    public boolean isEmpty() {
+        return spotsTaken == 0;
     }
 
-    public int getAmountOfVacantSpots() {
-        return warehouse.length - spotID;
+    public int getSize() {
+        return maxWarehouseCapacity;
     }
 
-    public int getAmountOfSpots() {
-        return warehouse.length;
+    public int getIndex(ContainerUnit unit) {
+        for (int i = 0; i < loadNumber; i++) {
+            try {
+                if (warehouse[i].equals(unit))
+                    return i;
+            } catch (NullPointerException e) {
+            }
+        }
+        return -1;
+    }
+
+    public ContainerUnit getContainerByIndex(int index) {
+        ContainerUnit unit;
+        try {
+            unit = warehouse[index];
+            return unit;
+        } catch (NullPointerException e) {
+            View.view("Container with index " + index + " doesn't exist");
+            return null;
+        }
     }
 
     public boolean isFull() {
-        return warehouse.length - spotID == 0;
+        return maxWarehouseCapacity == spotsTaken;
     }
 
-    public boolean isEmpty() {
-        return spotID == 0;
-    }
-
-    public int size() {
-        return warehouse.length;
-    }
-
-    public void add(Container unit) {
-        if (warehouse.length == spotID)
+    public void add(ContainerUnit unit) {
+        if (warehouse.length == loadNumber)
             addMoreSpaceToArray();
-        warehouse[spotID] = unit;
-        spotID++;
+        warehouse[loadNumber] = unit;
+        loadNumber++;
+        spotsTaken++;
     }
 
-    public void addMultiple(Container... unit) {
-        for (Container x : unit) {
+    public void addMultiple(ContainerUnit... unit) {
+        for (ContainerUnit x : unit) {
             add(x);
         }
     }
 
-    /*
-    public boolean checkIfContainedByContainerID(int containerIDtoCheck) {
-        boolean result = false;
-        for (Container cont : warehouse) {
-            if (cont.getContainerID() == containerIDtoCheck)
-                result = true;
+    public void setMaxWarehouseCapacity(int maxWarehouseCapacity) {
+        this.maxWarehouseCapacity = maxWarehouseCapacity;
+    }
+
+    public int getMaxWarehouseCapacity() {
+        return maxWarehouseCapacity;
+    }
+
+    public boolean contains(ContainerUnit unit) {
+        return getIndex(unit) != -1;
+    }
+
+    public boolean containsSeveral(ContainerUnit... units) {
+        boolean res = true;
+        for (ContainerUnit i : units) {
+            if (getIndex(i) == -1)
+                res = false;
         }
-        return result;
-    }
-
-    public boolean checkSeveralIfContainedByContainerIDs(int... containerIDstoCheck) {
-        int num = containerIDstoCheck.length;
-        int res = 0;
-        for (Container cont : warehouse) {
-            for (int i : containerIDstoCheck) {
-                if (cont.getContainerID() == i)
-                    res++;
-            }
-        }
-        return num == res;
-    }
-
-    */
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+        return res;
     }
 
     public void addMoreSpaceToArray() {
-        Container[] warehouseReplaser = (Container[]) new Object[warehouse.length + ARRAY_INCREASE_SIZE];
-
+        ContainerUnit[] warehouseReplaser = (ContainerUnit[]) new Object[warehouse.length + ARRAY_INCREASE_SIZE];
         for (int i = 0; i < warehouse.length; i++) {
             warehouseReplaser[i] = warehouse[i];
         }
         warehouse = warehouseReplaser;
     }
 
-    public void remove(int index) {
+    public void removeByID(int index) {
         warehouse[index] = null;
+        spotsTaken--;
+    }
+
+    public int getNextLoadNumber() {
+        return loadNumber;
     }
 
     public void eraseBase() {
         warehouse = null;
-        spotID = 0;
+        spotsTaken = 0;
     }
 
-    public boolean contains(Object unit) {
-        return false;
+    @Override
+    public int hashCode() {
+        return 42 * (this.getMaxWarehouseCapacity() + this.getTakenSpotsQuantity() + this.getNextLoadNumber());
+    }
+
+    @Override
+    public String toString() {
+        return "Warehouse has capacity of " + maxWarehouseCapacity + " spots, currently has " + getVacantSpaces() +
+                " vacant spaces.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Warehouse<ContainerUnit> warehouse1 = (Warehouse<ContainerUnit>) o;
+        if (loadNumber != warehouse1.loadNumber) return false;
+        if (spotsTaken != warehouse1.spotsTaken) return false;
+        if (maxWarehouseCapacity != warehouse1.maxWarehouseCapacity) return false;
+        return Arrays.equals(warehouse, warehouse1.warehouse);
     }
 }
-
