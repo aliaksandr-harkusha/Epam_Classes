7010257b1e440bd1f5d6772bb383b7209eea05de
Mafyak <Huba.est@gmail.com>
2018-02-27 22:13:54 +0300

    Create Controller.java

---
+++ Pre-Training/Task 6/Controller/Controller.java
@@ -0,0 +1,10 @@
+package by.epam.preTraining.SiarheiHuba.tasks.task6.Controller;
+
+import by.epam.preTraining.SiarheiHuba.tasks.task6.Model.Stack;
+
+public class Controller {
+    public static void main(String[] args) {
+        Stack tester = new Stack();
+        System.out.println(tester.isThisStringPolindrome("stuff"));
+    }
+}
