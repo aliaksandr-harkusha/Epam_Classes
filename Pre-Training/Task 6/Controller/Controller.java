8c4f904cf609514f94759c72088f8c41b952f8f5
Mafyak <Huba.est@gmail.com>
2018-02-27 22:14:39 +0300

    Update Controller.java

--- Pre-Training/Task 6/Controller/Controller.java
+++ Pre-Training/Task 6/Controller/Controller.java
@@ -5,6 +5,6 @@ import by.epam.preTraining.SiarheiHuba.tasks.task6.Model.Stack;
 public class Controller {
     public static void main(String[] args) {
         Stack tester = new Stack();
-        System.out.println(tester.isThisStringPolindrome("stuff"));
+        System.out.println(tester.checkIfThisStringPolindrome("stuff"));
     }
 }
