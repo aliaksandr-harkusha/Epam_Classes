98724034b8148f1e95e3ee326b66f4f19c2a9dc5
Mafyak <Huba.est@gmail.com>
2018-04-20 13:42:47 +0300

    Create Gloves.java

---
+++ Training/Task1_my/AssetClasses/Gear/Gloves.java
@@ -0,0 +1,13 @@
+package Task1.Model.AssetClasses.Gear;
+
+public class Gloves extends MotorcycleGear {
+    private String type = "Gloves";
+    private String model;
+
+    public Gloves(){
+    }
+
+    public Gloves(double price, double weight){
+        super(price, weight);
+    }
+}
